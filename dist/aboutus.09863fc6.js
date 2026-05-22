7557c3e737d0675858b05e5d66040568015cfdd1
Sercan Şimşek <sercansimsek44@gmail.com>
2026-05-22 12:04:28 +0200

    Custom javascript part 1

--- dist/aboutus.09863fc6.js
+++ dist/aboutus.09863fc6.js
@@ -1,20 +1,19 @@
 console.log("javascript connected!");
-
 const carousel = new bootstrap.Carousel("#homeCarousel", {
-  interval: 2000,
-  pause: false,
+    interval: 2000,
+    pause: false
 });
-
 // when the pause button is clicked, pause the carousel
 const carouselPause = document.getElementById("carouselPause");
-carouselPause.addEventListener("click", function () {
-  console.log("pausing the carousel");
-  carousel.pause();
+carouselPause.addEventListener("click", function() {
+    console.log("pausing the carousel");
+    carousel.pause();
 });
-
 // when the play button is clicked, begin cycling through the images
 const carouselPlay = document.getElementById("carouselPlay");
-carouselPlay.addEventListener("click", function () {
-  console.log("cycle the carousel");
-  carousel.cycle();
+carouselPlay.addEventListener("click", function() {
+    console.log("cycle the carousel");
+    carousel.cycle();
 });
+
+//# sourceMappingURL=aboutus.09863fc6.js.map
